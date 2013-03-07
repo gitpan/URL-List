9231f95560383d65a8b2366cf9cc96333fb597f5
Tore Aursand <toreau@cpan.org>
2013-03-07 13:57:28 +0000

    Import of TOREAU/URL-List-0.03 from CPAN.
    gitpan-cpan-distribution: URL-List
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         TOREAU/URL-List-0.03.tar.gz
    gitpan-cpan-author:       TOREAU
    gitpan-cpan-maturity:     released

--- lib/Changes.pm
+++
@@ -1,5 +0,0 @@
-use strict;
-use warnings;
-package Changes;
-
-1;
--- lib/URL/List.pm
+++ lib/URL/List.pm
@@ -8,11 +8,11 @@ URL::List - Object-oriented methods of handling list of URLs.
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use URI;
 use Domain::PublicSuffix;
